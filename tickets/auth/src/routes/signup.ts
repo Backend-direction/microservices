721f6bd32bc8c2332df2c44f4b918b5d16ec053b
volodymyrpankiv <volodymyr.pankiv@teck.com>
2022-06-30 11:01:24 +0300

    added unit test to auth service:

--- tickets/auth/src/routes/signup.ts
+++ tickets/auth/src/routes/signup.ts
@@ -3,21 +3,24 @@ import { body } from 'express-validator';
 import jwt from 'jsonwebtoken';
 
 import { BadRequestError } from '../errors/bad-request-error';
+import { validateRequest } from '../middlewares/validate-request';
 import { User } from '../models/user';
 
 const router = expres.Router();
 
-router.post('/api/users/signup',
- [
-   body('email')
-     .isEmail()
-     .withMessage('Email must be valid'),
-   body('password')
-     .trim()
-     .isLength({ min: 4, max: 20})
-     .withMessage('Password must between 4 and 20 chars'),
- ],
- async (req: Request, res: Response) => {
+router.post(
+  '/api/users/signup',
+  [
+    body('email')
+      .isEmail()
+      .withMessage('Email must be valid'),
+    body('password')
+      .trim()
+      .isLength({ min: 4, max: 20})
+      .withMessage('Password must between 4 and 20 chars'),
+  ],
+  validateRequest,
+  async (req: Request, res: Response) => {
   const { email, password } = req.body;
 
   const existingUser = await User.findOne({ email });
